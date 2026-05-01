9e6ea43bc9f47ad9197d56624429507c6d7adefc
codewithwahib <wahibjaffer043@gmail.com>
2026-05-01 17:09:01 +0500

    QQQ

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',  // Disabled for Sanity Studio support
   images: {
-    unoptimized: true,  // Required for static export (keep if needed for images)
+    unoptimized: true,
   },
-  trailingSlash: true,  // Adds trailing slashes to URLs
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
